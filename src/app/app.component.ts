3fb68e8a9eb11bc2f6f2999a773d3402a2455def
Kairuus <kingkairuus@gmail.com>
2023-06-27 21:17:50 -0300

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
-export class AppComponent {
-  title = 'christmas-lights';
+export class AppComponent implements OnInit{
+
+
+
+  ngOnInit(): void {
+
+  }
+
+  turnOn(){
+    let container = document.querySelector('.container')
+    container?.classList.add('on')
+  }
+
+  turnOff(){
+    let container = document.querySelector('.container')
+    container?.classList.remove('on')
+  }
+
 }
+
